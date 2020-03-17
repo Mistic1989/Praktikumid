00996d4a6dfcbbc494ab8f8c582bb9595df102ca
Mistic1989 <alo_ansberg@hotmail.com>
2020-03-17 17:08:03 +0200

    tere2

--- Praktikum4/Ülesanne 7/src/Lühendaja.java
+++ Praktikum4/Ülesanne 7/src/Lühendaja.java
@@ -3,12 +3,15 @@ import java.io.PrintWriter;
 import java.io.File;
 
 public class Lühendaja {
-    File fail = new java.io.File("c:/temp/nimed.txt");
-    Scanner sc = new Scanner(fail, "UTF-8");
-    while (sc.hasNextLine()) {
-        String rida = sc.nextLine();
-        String[] tükid = rida.split(" ");
-        System.out.print(tükid[tükid.length-2]+" "+tükid[tükid.length-1]);
-        System.out.println();
+
+    public static void main(String[] args) throws Exception {
+        File fail = new java.io.File("c:/coffee/nimed.txt");
+        Scanner sc = new Scanner(fail, "UTF-8");
+        while (sc.hasNextLine()) {
+            String rida = sc.nextLine();
+            String[] tükid = rida.split(" ");
+            System.out.print(tükid[tükid.length-2]+" "+tükid[tükid.length-1]);
+            System.out.println();
+        }
     }
 }
