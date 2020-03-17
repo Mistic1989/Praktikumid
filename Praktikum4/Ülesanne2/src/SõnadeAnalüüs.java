42ec3d344f196a45b1065b5011539681deecbaa1
Mistic1989 <alo_ansberg@hotmail.com>
2020-03-17 17:02:30 +0200

    tere2

--- Praktikum4/Ülesanne2/src/SõnadeAnalüüs.java
+++ Praktikum4/Ülesanne2/src/SõnadeAnalüüs.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+import java.io.PrintWriter;
+import java.io.File;
+
 public class SõnadeAnalüüs {
     public void analüüs(String a) {
         String[] tükid = a.split(" ");
@@ -6,8 +10,55 @@ public class SõnadeAnalüüs {
             System.out.println(tükid[i] + " - sõna pikkus: " + tükid[i].length());
         }
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SõnadeAnalüüs uus = new SõnadeAnalüüs();
         uus.analüüs("Tere kuidas siis päev on ka möödunud?");
+
+
+        char[] sümboliteMassiiv = {'T','e','r','e'};
+        String teade1 = new String(sümboliteMassiiv);
+        String teade2 = "Tere";
+        String teade3 = "Tere";
+
+
+        System.out.println(teade1.equals(teade2));
+        System.out.println(teade2.equals(teade3));
+        System.out.println(teade1 == teade2);
+        System.out.println(teade2 == teade3);
+
+        int[] arvud = { 3, 5, 7, 11 };
+        StringBuilder sb = new StringBuilder(50); // küsime 50 tähemärgi jagu puhvrit
+        sb.append("esimesed algarvud on ");
+        for (int arv : arvud) {
+            sb.append(arv);
+            sb.append(", ");
+        }
+        sb.insert(34, 100);
+        String tulemus = sb.toString();
+        System.out.println(tulemus);
+
+        File fail = new File("c:/coffee/marsruut.txt");
+
+        if (fail.createNewFile())
+            System.out.println("Fail loodud");
+        else
+            System.out.println("Faili on juba olemas" + "\n");
+
+        PrintWriter pw = new PrintWriter(fail, "UTF-8");
+        pw.print("Karl Ernst von Baeri ");
+        pw.println("tänav");
+        pw.print("Johann Wilhelm Friedrich Hezeli ");
+        pw.println("tänav");
+        pw.print("Juhan Liivi ");
+        pw.println("tänav");
+
+        pw.close();
+
+        Scanner tere = new Scanner(fail);
+        while (tere.hasNextLine()) {
+            String data = tere.nextLine();
+            System.out.println(data);
+        }
+
     }
 }
