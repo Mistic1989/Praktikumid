43080cfe20e136bbf6050e02e4e228ee7a78a995
Mistic1989 <alo_ansberg@hotmail.com>
2020-07-17 17:04:56 +0300

    1

--- Praktikum4/Id-kaart/src/Idkaart.java
+++ Praktikum4/Id-kaart/src/Idkaart.java
@@ -1,13 +1,91 @@
 public class Idkaart {
+
+    private String idnumber;
+    private String nimi;
+    private String isikukood;
+
+
+    public int sünnikuupäev() {
+        int algus = Integer.parseInt(isikukood.substring(0, 1));
+        int aasta = Integer.parseInt(isikukood.substring(1, 3));
+        if (algus <= 2)
+            return 1800 + aasta;
+        if (algus <= 4)
+            return 1900 + aasta;
+        if (algus <= 6)
+            return 2000 + aasta;
+        return 2100 + aasta;
+    }
+
     public String sugu() {
+        String a1 = isikukood.substring(0, 1);
+
+        if (a1.equals("1"))
+            return "Sugu: mees";
+        if (a1.equals("3"))
+            return "Sugu: mees";
+        if (a1.equals("5"))
+            return "Sugu: mees";
+
+        if (a1.equals("2"))
+            return "Sugu: naine";
+        if (a1.equals("4"))
+            return "Sugu: naine";
+        if (a1.equals("6"))
+            return "Sugu: naine";
+        return "Viga";
+    }
+/*
+        String[] isikukoodnr = isikukood.split("");
+        String[] varem = new String[3];
+        
+        String aasta = isikukoodnr[1] + isikukoodnr[2];
+        String kuu = isikukoodnr[3] + isikukoodnr[4];
+        String päev = isikukoodnr[5] + isikukoodnr[6];
+        String aasta1900 = "19";
+        String aasta2000 = "20";
+        String a = aasta1900 + aasta;
+        String b = aasta2000 + aasta;;
+
+        if (isikukoodnr[0] == "3" || isikukoodnr[0] == "4") {
+            varem[0] = päev;
+            varem[1] = kuu;
+            varem[2] = a;
+
+        }
+        if (isikukoodnr[0] == "5" || isikukoodnr[0] == "6") {
+            varem[0] = päev;
+            varem[1] = kuu;
+            varem[2] = b;
+
+        }
+        return varem[0] + varem[1] + varem[2];*/
+    /*}
+
+
+
+
+    /*
+        String[] isikukoodnr = isikukood.split("");
+        String[] suguarr = new String[1];
         String mees = "Sugu: mees";
         String naine = "Sugu: naine";
-        if (isikukood[0] = 1 || isikukood[0] = 3 || isikukood[0] = 5) {
-            return mees;
-            if (isikukood[0] = 2 || isikukood[0] = 4 || isikukood[0] = 6) {
-                return naine;
-            };
+        if (isikukoodnr[0] == "1" || isikukoodnr[0] == "3" || isikukoodnr[0] == "5") {
+            suguarr[0] = mees;
         }
+            if (isikukoodnr[0] == "2" || isikukoodnr[0] == "4" || isikukoodnr[0] == "6") {
+                suguarr[0] = naine;
+        }
+            return suguarr[0];
+    }*/
+
+    @Override
+    public String toString() {
+        return "Idkaart{" +
+                "idnumber='" + idnumber + '\'' +
+                ", nimi='" + nimi + '\'' +
+                ", isikukood='" + isikukood + '\'' +
+                '}';
     }
 
     public Idkaart(String idnumber, String nimi, String isikukood) {
@@ -40,7 +118,5 @@ public class Idkaart {
         this.isikukood = isikukood;
     }
 
-    private String idnumber;
-    private String nimi;
-    private String isikukood;
+
 }
---
+++ Praktikum4/Id-kaart/src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        Idkaart idkaart1 = new Idkaart("31546845", "Alo Ansberg", "38902184911");
+        System.out.println(idkaart1.sugu());
+        System.out.println(idkaart1.sünnikuupäev());
+        System.out.println(idkaart1);
+    }
+}
